4db89036cd2a5be5a782aa5bc7a0d9609a152906
Leo Sarrazin <saqrcheiron@gmail.com>
2026-01-15 22:44:27 +0100

    fix: missing

--- src/traits/CameraObservable.ts
+++ src/traits/CameraObservable.ts
@@ -2,19 +2,19 @@ import * as THREE from "three";
 import { trait } from "../traits";
 
 export type CameraConfig = {
-  type: string;
-  radius: number;
-  trackRotation?: boolean;
-  minDistance?: number;
-  maxDistance?: number;
-  getObjectRotation: () => THREE.Quaternion;
+    type: string;
+    radius: number;
+    trackRotation?: boolean;
+    minDistance?: number;
+    maxDistance?: number;
+    getObjectRotation: () => THREE.Quaternion;
 };
 
 // Trait example pattern: call `CameraObservable.from(instance)` to project the trait.
 export class CameraObservable {
-  static from = trait(CameraObservable);
+    static from = trait(CameraObservable);
 
-  getCameraConfig!: () => CameraConfig;
+    getCameraConfig!: () => CameraConfig;
 }
 
 export type CameraObservableTrait = ReturnType<typeof CameraObservable.from>;
